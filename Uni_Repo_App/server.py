4b0b65621ce82afba31d0054e50ea55ba14f0002
sushmithr <sushmithramesh@gmail.com>
2019-11-24 19:45:00 -0500

    added docstrings and spacing

--- Uni_Repo_App/server.py
+++ Uni_Repo_App/server.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-    Testing Flask Installation
+Created on Sun Nov 24 6:31 PM 2019
+
+@author: Sushmith Ramesh
+
+Implemented a Python Flask application that uses Jinja2 & sqlite3 to get the appropriate information 
+about intructors from a database and render/display a table with said information, locally, on a browser
 """
+
 
 import sqlite3
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/instructors')
 def instructors_summary():
+    """ This route attempts to connect to the sqlite3 database & query for information about 
+        instructors after which it renders an html page with the information displayed in a table 
+    """
     dbpath = r'C:\Sushmith Ramesh\SSW_810_Python\Py_Uni_Repo_Proj\Python_University_Repository\Uni_Repo_App\810_startup.db'
 
     try:
@@ -32,5 +45,6 @@ def instructors_summary():
                     page_header="Stevens Repository", 
                     table_title="Courses and student counts",
                     instructors=data)
+
 
 app.run(debug=True)
